4a0a1715cd5364565f71c01f7d12449cbf6d2abe
vitalii-borisiuk <vitalii.borisiuk@gmail.com>
2023-01-05 10:31:37 +0200

    Edit hw-js-08 part 3
    Исправление ошибок домашней работы hw-js-08 3-го задания.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,42 @@
 const STORAGE_MESSAGE = 'save-input';
-const formData = {};
 
 const refs = {
     form: document.querySelector('.feedback-form'),
     email: document.querySelector('.feedback-form input'),
     textarea: document.querySelector('.feedback-form textarea'),
-
 }
+
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', onTextareaInput);
 messageOutput();
+    
+const formData = {
+    email: refs.email.value,
+    message:refs.textarea.value,
+};
 
 function onFormSubmit(e) {
     e.preventDefault();
+        if (refs.email.value === '' || refs.textarea.value === '') {
+        return alert('Введіть всі поля форми');
+    }
+    console.log(formData);
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_MESSAGE);
 };
 
 function onTextareaInput(e) {
+
     formData[e.target.name] = e.target.value;
-    const data = JSON.stringify(formData);   
-    localStorage.setItem(STORAGE_MESSAGE, data);
-        
+    localStorage.setItem(STORAGE_MESSAGE, JSON.stringify(formData));    
 };
 
 function messageOutput() {
-    const savedMessage = localStorage.getItem(STORAGE_MESSAGE);
-    const fromStorage = JSON.parse(savedMessage);
-    
-    if (fromStorage) { 
+    const fromStorage = JSON.parse(localStorage.getItem(STORAGE_MESSAGE));
+     if (fromStorage) {
         refs.email.value = fromStorage.email;
         refs.textarea.value = fromStorage.message;
     };
+
 };
